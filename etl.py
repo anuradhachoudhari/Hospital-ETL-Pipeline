8cb77976377fedfc3444c1ab16bca605c0ecaf7a
anuradhachoudhari <its.anuradha.choudhari@gmail.com>
2026-03-04 22:11:02 +0530

    Initial commit - Hospital ETL pipeline using Python and MySQL

---
+++ etl.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import mysql.connector
+
+# Read Excel file
+df = pd.read_excel("hospital_data.xlsx")
+
+# Connect to MySQL
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Enhypen_bts@2",
+    database="hospital_db"
+)
+
+cursor = conn.cursor()
+
+for index, row in df.iterrows():
+    cursor.execute("""
+        INSERT INTO Patients (age, gender, blood_type)
+        VALUES (%s, %s, %s)
+    """, (row['Age'], row['Gender'], row['Blood Type']))
+
+conn.commit()
+conn.close()
+
+print("Data inserted successfully!")
